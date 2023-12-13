d7d35f58ae17cf56c13d18277be9508971da3b33
Sam-rst <samuel.ressiot@gmail.com>
2023-12-13 15:00:02 +0100

    Blabla

---
+++ PaulFolder/test.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire en PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<?php
+// FAIT PAR PAUL LE GOAT
+class FormulaireData
+{
+    private $formSubmitted = false;
+    private $data = [];
+
+    public function __construct()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $this->data = [
+                'firstname' => $_POST["firstname"],
+                'lastname' => $_POST["lastname"],
+                'password' => $_POST["password"],
+                'email' => $_POST["email"],
+                'phone' => $_POST["phone"],
+                'birthday' => $_POST["birthday"],
+                'description' => $_POST["description"]
+            ];
+
+            $this->handleFormSubmission();
+        }
+    }
+
+    public function handleFormSubmission()
+    {
+        if ($this->validateFormData()) {
+            $this->formSubmitted = $this->insertData();
+        } else {
+            echo '<div class="error-message message">Veuillez remplir correctement tous les champs du formulaire.</div>';
+        }
+    }
+
+    public function validateFormData():bool
+    {
+        foreach ($this->data as $key => $value) {
+            if (empty($value)) {
+                return false;
+            }
+            $this->data[$key] = trim($value);
+            $this->data[$key] = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            if (empty($this->data[$key])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function insertData()
+    {
+        $servername = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbname = "Php_Secure_Paul";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbUsername, $dbPassword);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $checkStmt = $conn->prepare("SELECT * FROM formulaire_data WHERE email = ?");
+            $checkStmt->execute([$this->data['email']]);
+            $checkResult = $checkStmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($checkResult) {
+                echo '<div class="error-message message">L\'email est déjà utilisé</div>';
+            } else {
+                $insertStmt = $conn->prepare("INSERT INTO formulaire_data (firstname, lastname, password, email, phone, birthday, description) VALUES (:firstname, :lastname, :password, :email, :phone, :birthday, :description)");
+
+                $insertStmt->bindParam(':firstname', $this->data['firstname'], PDO::PARAM_STR);
+                $insertStmt->bindParam(':lastname', $this->data['lastname'], PDO::PARAM_STR);
+                $insertStmt->bindParam(':password', $this->data['password'], PDO::PARAM_STR);
+                $insertStmt->bindParam(':email', $this->data['email'], PDO::PARAM_STR);
+                $insertStmt->bindParam(':phone', $this->data['phone'], PDO::PARAM_STR);
+                $insertStmt->bindParam(':birthday', $this->data['birthday'], PDO::PARAM_STR);
+                $insertStmt->bindParam(':description', $this->data['description'], PDO::PARAM_STR);
+
+                $insertStmt->execute();
+            }
+        } catch (PDOException $e) {
+            echo '<div class="error-message message">Erreur de connexion à la base de données: ' . $e->getMessage() . '</div>';
+        } finally {
+            $conn = null;
+        }
+
+        return false;
+    }
+
+    public function displaySuccessMessage()
+    {
+        if ($this->formSubmitted) {
+            echo '<div class="success-message message">Informations soumises avec succès !</div>';
+        }
+    }
+
+    public function displaySubmittedData()
+    {
+        if ($this->formSubmitted) {
+            echo '<div class="submitted-data">';
+            echo '<h2>Données soumises :</h2>';
+            foreach ($this->data as $key => $value) {
+                echo "<p>$key : $value</p>";
+            }
+            echo '</div>';
+        }
+    }
+}
+
+$FormulaireData = new FormulaireData();
+
+?>
+
+    <form id="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <h2>Formulaire</h2>
+
+        <div class="form-control">
+            <label for="firstname">Prénom:</label>
+            <input type="text" name="firstname" pattern="[A-Za-zÀ-ÖØ-öø-ÿ\s]+" title="Veuillez entrer un prénom valide" required>
+            <small>Error message</small>
+        </div>
+
+        <div class="form-control">
+            <label for="lastname">Nom:</label>
+            <input type="text" name="lastname" pattern="[A-Za-zÀ-ÖØ-öø-ÿ\s]+" title="Veuillez entrer un nom valide" required>
+            <small>Error message</small>
+        </div>
+
+        <div class="form-control">
+            <label for="password">Mot de passe:</label>
+            <input type="password" name="password" pattern=".{5,}" title="Le mot de passe doit contenir au moins 8 caractères" required>
+            <small>Error message</small>
+        </div>
+
+        <div class="form-control">
+            <label for="email">Email:</label>
+            <input type="email" name="email" required>
+            <small>Error message</small>
+        </div>
+
+
+        <div class="form-control">
+            <label for="phone">Numéro de téléphone:</label>
+            <input type="tel" name="phone" pattern="[0-9]{10}" title="Veuillez entrer un numéro de téléphone valide" required>
+            <small>Error message</small>
+        </div>
+
+        <div class="form-control">
+            <label for="birthday">Date de naissance:</label>
+            <input type="date" name="birthday" required>
+            <small>Error message</small>
+        </div>
+
+        <div class="form-control">
+            <label for="description">Description:</label>
+            <textarea id="description" name="description" rows="4" cols="50" required></textarea>
+            <small>Error message</small>
+        </div>
+
+        <input type="submit" value="Soumettre">
+
+    </form>
+
+    <?php
+    $FormulaireData->displaySuccessMessage();
+    $FormulaireData->displaySubmittedData();
+    ?>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire en PHP</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column; /* Mettez en colonne les éléments à l'intérieur du body */
+            align-items: center;
+            height: 100vh;
+        }
+
+        form {
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 300px;
+            margin-bottom: 20px; /* Ajout d'une marge en bas pour espacer le formulaire de la section suivante */
+            margin-top : 20px;
+        }
+
+        h2 {
+            text-align: center;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            color: #333;
+        }
+
+        input, textarea {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 12px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        textarea {
+            resize: vertical;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: white;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        .success-message {
+            color: #008000;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        .error-message {
+            color: #FF0000;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        .submitted-data {
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 300px;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+// Initialiser la variable d'état du formulaire
+$formSubmitted = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les valeurs du formulaire
+    $firstname = validateInput($_POST["firstname"]);
+    $lastname = validateInput($_POST["lastname"]);
+    $password = validateInput($_POST["password"]);
+    $email = validateInput($_POST["email"]);
+    $phone = validateInput($_POST["phone"]);
+    $birthday = validateInput($_POST["birthday"]);
+    $description = validateInput($_POST["description"]);
+
+    // Connexion à la base de données (remplacez les valeurs par celles de votre configuration)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Php_Secure_Paul";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Vérifier la connexion à la base de données
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Préparer la requête d'insertion
+    $stmt = $conn->prepare("INSERT INTO formulaire_data (firstname, lastname, password, email, phone, birthday, description) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssss", $firstname, $lastname, $password, $email, $phone, $birthday, $description);
+
+    // Exécuter la requête
+    if ($stmt->execute()) {
+        $formSubmitted = true;
+    }
+
+    // Fermer la connexion
+    $stmt->close();
+    $conn->close();
+}
+
+// Fonction pour valider les entrées
+function validateInput($data) {
+    // Échapper les caractères spéciaux pour éviter les attaques XSS
+    $data = htmlspecialchars($data);
+    // Nettoyer les espaces inutiles
+    $data = trim($data);
+    // Supprimer les antislashs pour éviter les attaques par injection
+    $data = stripslashes($data);
+    return $data;
+}
+
+// Fonction pour valider l'adresse email
+function validateEmail($email) {
+    // Échapper les caractères spéciaux pour éviter les attaques XSS
+    $email = htmlspecialchars($email);
+    // Valider l'adresse email avec filter_var
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    return $email;
+}
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <h2>Formulaire</h2>
+    
+    <!-- Champ pour le prénom -->
+    <!-- Prénom (Firstname):
+        pattern="[A-Za-zÀ-ÖØ-öø-ÿ\s]+": Ce pattern autorise uniquement des lettres (majuscules et minuscules), des espaces et des caractères accentués pour le prénom. -->
+    <label for="firstname">Prénom:</label>
+    <input type="text" name="firstname" pattern="[A-Za-zÀ-ÖØ-öø-ÿ\s]+" title="Veuillez entrer un prénom valide" required>
+
+
+    <!-- Champ pour le nom -->
+    <!-- Nom (Lastname):
+        pattern="[A-Za-zÀ-ÖØ-öø-ÿ\s]+": Semblable au prénom, ce pattern autorise uniquement des lettres (majuscules et minuscules), des espaces et des caractères accentués pour le nom. -->
+    <label for="lastname">Nom:</label>
+    <input type="text" name="lastname" pattern="[A-Za-zÀ-ÖØ-öø-ÿ\s]+" title="Veuillez entrer un nom valide" required>
+
+
+    <!-- Champ pour le mot de passe -->
+    <!-- Mot de passe (Password):
+        pattern=".{8,}": Ce pattern spécifie que le mot de passe doit contenir au moins 8 caractères. -->
+    <label for="password">Mot de passe:</label>
+    <input type="password" name="password" pattern=".{5,}" title="Le mot de passe doit contenir au moins 8 caractères" required>
+
+
+    <!-- Champ pour l'email -->
+    <label for="email">Email:</label>
+    <input type="email" name="email" required>
+
+
+    <!-- Champ pour le numéro de téléphone -->
+    <!-- Numéro de téléphone (Phone):
+        type="tel": Utilisation du type d'entrée téléphone pour indiquer que le champ doit contenir un numéro de téléphone.
+        pattern="[0-9]{10}": Ce pattern spécifie que le numéro de téléphone doit contenir exactement 10 chiffres. -->
+    <label for="phone">Numéro de téléphone:</label>
+    <input type="tel" name="phone" pattern="[0-9]{10}" title="Veuillez entrer un numéro de téléphone valide" required>
+
+    <!-- Champ pour la date de naissance -->
+    <label for="birthday">Date de naissance:</label>
+    <input type="date" name="birthday" required>
+
+    <!-- Champ pour la description -->
+    <label for="description">Description:</label>
+    <textarea name="description" rows="4" cols="50" required></textarea>
+
+    <!-- Bouton de soumission du formulaire -->
+    <input type="submit" value="Soumettre">
+    
+    <?php
+    // Afficher le message de succès ou d'erreur en fonction de l'état du formulaire
+    // Afficher le message de succès ou d'erreur en fonction de l'état du formulaire
+    if ($formSubmitted) {
+        echo '<div class="success-message">Informations soumises avec succès !</div>';
+    } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+        echo '<div class="error-message">Veuillez remplir le formulaire.</div>';
+    }
+
+    ?>
+</form>
+
+<!-- Section pour afficher les données soumises -->
+<?php
+if ($formSubmitted) {
+    echo '<div class="submitted-data">';
+    echo '<h2>Données soumises :</h2>';
+    echo "<p>Prénom : $firstname</p>";
+    echo "<p>Nom : $lastname</p>";
+    echo "<p>Mot de passe : $password</p>";
+    echo "<p>Email : $email</p>";
+    echo "<p>Téléphone : $phone</p>";
+    echo "<p>Date de naissance : $birthday</p>";
+    echo "<p>Description : $description</p>";
+    echo '</div>';
+} else {}
+    ?>
+
+
+</body>
+</html>
+
+
